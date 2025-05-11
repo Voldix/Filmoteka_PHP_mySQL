7ed923eb68a0ed9e04945c9e612dd2fc7be57973
Voldix <antony.beam@yandex.ru>
2025-05-11 19:43:27 +0300

    v2

--- index.php
+++ index.php
@@ -142,7 +142,7 @@ if ($result = mysqli_query($link, $query)) {
     <script src="libs/jquery/jquery.min.js"></script><!-- endbuild -->
     <!-- build:jsVendor js/vendor.js -->
     <script src="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.js"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIr67yxxPmnF-xb4JVokCVGgLbPtuqxiA"></script><!-- endbuild -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key="></script><!-- endbuild -->
     <!-- build:jsMain js/main.js -->
     <script src="js/main.js"></script><!-- endbuild -->
     <script defer="defer" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
